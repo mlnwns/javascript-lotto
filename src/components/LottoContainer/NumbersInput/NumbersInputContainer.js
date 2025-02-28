c716d457c626dbce49dd7429e427881dcf5e2401
mlnwns <mlnwns@naver.com>
2025-02-28 15:41:42 +0900

    feat: 입력 필드를 모두 채울 시 결과 버튼 활성화

--- src/components/LottoContainer/NumbersInput/NumbersInputContainer.js
+++ src/components/LottoContainer/NumbersInput/NumbersInputContainer.js
@@ -4,15 +4,34 @@ import { ResultButton } from "./ResultButton.js";
 import { createElement } from "../../../utils/dom/createElement.js";
 
 export const NumbersInputContainer = () => {
-  const numbersInputContainer = createElement("div", {
+  const numbersInputContainer = createElement("form", {
     className: "numbers-input-container",
   });
+
+  const resultButton = ResultButton();
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    const inputs = numbersInputContainer.querySelectorAll("input");
+    const allFilled = Array.from(inputs).every(
+      (input) => input.value.trim() !== ""
+    );
+
+    resultButton.disabled = !allFilled;
+  };
 
   numbersInputContainer.append(
     InputGuideText("지난 주 당첨번호 6개와 보너스 번호 1개를 입력해주세요."),
     InputContainer(),
-    ResultButton()
+    resultButton
   );
+
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  numbersInputContainer.addEventListener("input", handleInputChange);
+  numbersInputContainer.addEventListener("submit", handleInputSubmit);
 
   return numbersInputContainer;
 };
